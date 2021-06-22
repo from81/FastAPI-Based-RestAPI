920904a4800b7c82034fdd88c3e9f9ec47014b61
Kai Hirota <kaiyhirota@gmail.com>
2021-06-23 02:26:13 +1000

    clean up config

--- app/config.py
+++ app/config.py
@@ -10,8 +10,6 @@ DB_PASSWORD = config("DB_PASSWORD", cast=Secret, default="postgres")
 DB_HOST = config("DB_HOST", cast=str, default="localhost")
 DB_PORT = config("DB_PORT", cast=str, default="5432")
 DB_NAME = config("DB_NAME", cast=str, default="geo")
-JWT_PRIVATE_KEY = config("JWT_PRIVATE_KEY", cast=str, default="june212021")
-
 DB_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # MAX_CONNECTIONS_COUNT: int = config("MAX_CONNECTIONS_COUNT", cast=int, default=10)
@@ -19,6 +17,7 @@ DB_URL = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME
 MIN_CONNECTIONS_COUNT = 5
 MAX_CONNECTIONS_COUNT = 100
 
-ALGORITHM = "HS256"
+JWT_PRIVATE_KEY = config("JWT_PRIVATE_KEY", cast=str, default="june212021")
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7 # one week
+ALGORITHM = "HS256"
 API_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJrYWl5aGlyb3RhQGdtYWlsLmNvbSIsImV4cCI6MTYyNDg3MzAzNX0.bE9CiNCp7rQXBq9jXtY7lkSm5fpbMrFUncF1E4yRMbs"
