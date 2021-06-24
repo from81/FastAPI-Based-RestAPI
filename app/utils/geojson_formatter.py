863b4ea035beb977b2e85941f3a3eb92c797a53d
Kai Hirota <kaiyhirota@gmail.com>
2021-06-24 21:46:21 +1000

    decoupled neighborhood_service and GeoJSONFormatter

---
+++ app/utils/geojson_formatter.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict
+
+import numpy as np
+
+class GeoJSONFormatter:
+    def __init__(self, data: Dict[str, Any]):
+        self.raw_data = self.processed_data = data
+        self.right_hand_rule()
+        self.round_decimal(6)
+
+    def right_hand_rule(self):
+        """
+        Section 3.1.6 https://datatracker.ietf.org/doc/html/rfc7946
+        
+        A linear ring MUST follow the right-hand rule with respect to the
+        area it bounds, i.e., exterior rings are counterclockwise, and
+        holes are clockwise.
+        """
+        data = self.processed_data['features'][0]['geometry']['coordinates'][0]
+        data = data[::-1]
+        self.processed_data['features'][0]['geometry']['coordinates'][0] = data
+
+    def round_decimal(self, n: int = 0):
+        data = self.processed_data['features'][0]['geometry']['coordinates'][0]
+        arr = np.array(data)
+        data = arr.round(n).tolist()
+        self.processed_data['features'][0]['geometry']['coordinates'][0] = data
+    
+    def get_processed_data(self) -> Dict[str, Any]:
+        return self.processed_data
--- data/data_script.py
+++ data/data_script.py
@@ -1,8 +1,12 @@
 import os
 import sys
 
+import geojson
 import geopandas as gpd
 # from sqlalchemy import create_engine
+
+sys.path.append("..")
+from app.utils.geojson_formatter import GeoJSONFormatter
 
 class GeoData:
     def __init__(self, path: str):
@@ -16,10 +20,16 @@ class GeoData:
         self.epsg8058 = self.epsg4326.copy().to_crs(epsg=8058)
 
     def to_geojson(self):
+        geojs = geojson.loads(self.epsg4326.to_json())
+        formatter = GeoJSONFormatter(geojs)
+        formatted_geojs = formatter.get_processed_data()
+
         fname, ext = os.path.splitext(self.input_file)
         output_dir = self.output_dir if self.output_dir else self.input_dir
         path = os.path.join(output_dir, fname + '.json')
-        self.epsg4326.to_file(path, driver="GeoJSON")
+        
+        with open(path, 'w') as f:
+            geojson.dump(formatted_geojs, f)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
